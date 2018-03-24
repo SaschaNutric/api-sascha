0a6449301c8b46512e3724b716596d50b47ffe00
Leo1305Pineda <leo1305pineda@gmail.com>
2018-03-24 12:39:45 -0400

    Agregando auto-config para la base de dato

--- scripts/configdb.sh
+++ scripts/configdb.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-credenciales="module.exports = {
-    client: \"pg\",
-	connection: {
-		host: \"$HOST\",
-		user: \"$USER\",
-		password: \"$PASSWORD\",
-		database: \"$DATABASE\"
-	}
-}"
-
 MODE='NULL'
 DATABASE='saschadb'
 
@@ -17,16 +7,27 @@ if [ "$MODE" = "NULL" ]; then
 	AUTO_CONFIG=$(psql postgres -c "\l $DATABASE" 2>/dev/null | grep "$DATABASE" | head -1 | cut -d" " -f2)
 	if [ "$AUTO_CONFIG" = "$DATABASE" ]; then
 		MODE='development'	
-	fi
-	MODE='production'	
+	else
+		MODE='production'
+	fi	
 fi
 
 if [ "$MODE" = "development" ]; then
-	
-	USER='postgres'
+	USER_DB='postgres'
 	HOST='localhost'
 	PORT='5432'
 	PASSWORD='1234'
+
+	credenciales="module.exports = {
+    	client: \"pg\",
+		connection: {
+			host: \"$HOST\",
+			user: \"$USER_DB\",
+			password: \"$PASSWORD\",
+			database: \"$DATABASE\"
+		}
+	}"
+
 	echo "Configuracion modo desarrollo";
 	res=$(psql postgres -c "\l $DATABASE" 2>/dev/null | grep "$DATABASE" | head -1 | cut -d" " -f2)
 	if ! [ "$res" = "$DATABASE" ]; then
@@ -38,10 +39,21 @@ if [ "$MODE" = "development" ]; then
 else
 	if [ "$MODE" = "production" ]; then
 		DATABASE='postgres://byqkxhkjgnspco:7f90354e72f531d4d0deb47be4fdfb68765244e8a5d97ca9c9f7f97c05a0a9a9@ec2-54-243-210-70.compute-1.amazonaws.com:5432/d7h3pnfqclegkn'
-		USER='byqkxhkjgnspco'
+		USER_DB='byqkxhkjgnspco'
 		HOST='ec2-54-243-210-70.compute-1.amazonaws.com'
 		PORT='5432'
 		PASSWORD='7f90354e72f531d4d0deb47be4fdfb68765244e8a5d97ca9c9f7f97c05a0a9a9'
+
+		credenciales="module.exports = {
+   			client: \"pg\",
+			connection: {
+				host: \"$HOST\",
+				user: \"$USER_DB\",
+				password: \"$PASSWORD\",
+				database: \"$DATABASE\"
+			}
+		}"
+		
 		echo "Configuracion modo produccion";
 		echo "Registrando credenciales_heroku para la base de dato";
 		echo "module.exports = "$credenciales>knexfile.js
