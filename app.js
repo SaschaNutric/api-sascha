a2da1169aba3866d1ba9d8bfe7b61885b47b1d92
Leo1305Pineda <leo1305pineda@gmail.com>
2018-03-24 15:08:09 -0400

    Actualizando app.js

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+process.env.PWD = process.cwd();
 app.use(express.static(process.env.PWD + '/'), routes);
 
 //app.use('/', routes);
