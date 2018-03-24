b207ab47264e57c1dae7ebb304e6bc9c3f423065
Leo1305Pineda <leo1305pineda@gmail.com>
2018-03-24 17:00:44 -0400

    Agregando config en Api

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(app.router);
 
 process.env.PWD = process.cwd();
 app.use('/', routes);
