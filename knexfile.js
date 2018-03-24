e72f04d21898ce401003afcbe1a39ca93b68f5aa
Leo1305Pineda <leo1305pineda@gmail.com>
2018-03-24 13:17:54 -0400

    Agregando config para la base de dato

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,9 @@
 module.exports = {
-    	client: "pg",
+   	client: "pg",
 		connection: {
-			host: "localhost",
-			user: "postgres",
-			password: "1234",
-			database: "saschadb"
+			host: "ec2-54-243-210-70.compute-1.amazonaws.com",
+			user: "byqkxhkjgnspco",
+			password: "7f90354e72f531d4d0deb47be4fdfb68765244e8a5d97ca9c9f7f97c05a0a9a9",
+			database: "postgres://byqkxhkjgnspco:7f90354e72f531d4d0deb47be4fdfb68765244e8a5d97ca9c9f7f97c05a0a9a9@ec2-54-243-210-70.compute-1.amazonaws.com:5432/d7h3pnfqclegkn"
 		}
 	}
