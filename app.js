9fafda8f8c392e7a676fe96347ca94caf4c33d37
Leo1305Pineda <leo1305pineda@gmail.com>
2018-03-24 14:28:03 -0400

    Agregando config data base a heroku

--- app.js
+++ app.js
@@ -24,6 +24,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public'));
+});
+
 app.use('/', routes);
 app.get('/favicon.ico', function(req, res) {
     res.status(204);
@@ -34,8 +38,6 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,22 @@
-module.exports = {
-   	client: "pg",
-		connection: {
-			host: "ec2-54-243-210-70.compute-1.amazonaws.com",
-			user: "byqkxhkjgnspco",
-			password: "7f90354e72f531d4d0deb47be4fdfb68765244e8a5d97ca9c9f7f97c05a0a9a9",
-			database: "d7h3pnfqclegkn"
+if (process.env.NODE_ENV === 'development') {
+	module.exports = {
+   		client: "pg",
+			connection: {
+				host: "localhost",
+				user: "postgres",
+				password: "1234",
+				database: "saschadb"
+			}
 		}
-	}
+} else if (process.env.NODE_ENV === 'production') {
+	module.exports = {
+   		client: "pg",
+			connection: {
+				host: "ec2-54-243-210-70.compute-1.amazonaws.com",
+				user: "byqkxhkjgnspco",
+				password: "7f90354e72f531d4d0deb47be4fdfb68765244e8a5d97ca9c9f7f97c05a0a9a9",
+				database: "d7h3pnfqclegkn"
+			}
+		}
+}
+
