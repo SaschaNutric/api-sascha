28c45ae271ce342d164e5f80c099197c9bd157a3
Leo1305Pineda <leo1305pineda@gmail.com>
2018-03-24 13:31:03 -0400

    Agregando config para la base de dato

--- knexfile.js
+++ knexfile.js
@@ -4,6 +4,6 @@ module.exports = {
 			host: "ec2-54-243-210-70.compute-1.amazonaws.com",
 			user: "byqkxhkjgnspco",
 			password: "7f90354e72f531d4d0deb47be4fdfb68765244e8a5d97ca9c9f7f97c05a0a9a9",
-			database: "postgres://byqkxhkjgnspco:7f90354e72f531d4d0deb47be4fdfb68765244e8a5d97ca9c9f7f97c05a0a9a9@ec2-54-243-210-70.compute-1.amazonaws.com:5432/d7h3pnfqclegkn"
+			database: "d7h3pnfqclegkn"
 		}
 	}
