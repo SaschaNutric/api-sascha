b712d63f371db07ebc773c361e1fc178a5605da0
Leo1305Pineda <leo1305pineda@gmail.com>
2018-05-05 12:42:11 -0400

    Agregando id_bloque_horario

--- src/controllers/bloque_horarios.js
+++ src/controllers/bloque_horarios.js
@@ -7,7 +7,7 @@ function getBloque_horarios(req, res, next) {
 	Bloque_horarios.query(function (qb) {
    		qb.where('bloque_horario.estatus', '=', 1);
 	})
-	.fetch({ columns: ['hora_inicio','hora_fin'] })
+	.fetch({ columns: ['id_bloque_horario','hora_inicio','hora_fin'] })
 	.then(function(data) {
 		if (!data)
 			return res.status(404).json({ 
