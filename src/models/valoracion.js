9c4324a472e9b581b6b19d7f114a470556336305
Sascha <37225508+SaschaNutric@users.noreply.github.com>
2018-05-06 04:37:15 -0400

    Update valoracion.js

--- src/models/valoracion.js
+++ src/models/valoracion.js
@@ -1,10 +1,14 @@
 'use strict'
 
 const Bookshelf = require('../commons/bookshelf');
+const TipoValoracion = require('./tipo_valoracion');
 
 let Valoracion = Bookshelf.Model.extend({
   tableName: 'valoracion',
-  idAttribute: 'id_valoracion'
+  idAttribute: 'id_valoracion',
+    tipo_valoracion: function() {
+    return this.belongsTo(TipoValoracion, 'id_tipo_valoracion');
+  }
 });
 
 module.exports = Bookshelf.model('Valoracion', Valoracion);
