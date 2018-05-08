2a2096b64668e07f7d9de596d572374469625865
Jose Guerrero <guerrero.c.jose.a@gmail.com>
2018-05-08 13:48:23 -0430

    Resuelve error de merging en modelo suplemento

--- src/models/suplemento.js
+++ src/models/suplemento.js
@@ -1,11 +1,7 @@
 'use strict'
 
 const Bookshelf = require('../commons/bookshelf');
-<<<<<<< HEAD
-const Unidad = require('./unidad');
-=======
 const Unidad    = require('./unidad');
->>>>>>> 129974c6e304539abb7a7704987c9bd15e68db3f
 
 let Suplemento = Bookshelf.Model.extend({
   tableName: 'suplemento',
