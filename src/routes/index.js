ed9477b9518b5c79ed5beef794fc09cd66bdb3a5
Jose Guerrero <guerrero.c.jose.a@gmail.com>
2018-05-11 06:39:56 -0430

    Agrega Login para empleados en la intranet

--- src/routes/index.js
+++ src/routes/index.js
@@ -537,6 +537,7 @@ module.exports = (function () {
   api.put('/usuario/:id',                    usuariosCtrl.updateUsuario);
   api.delete('/usuario/:id',                 usuariosCtrl.deleteUsuario);
   api.post('/login',                         usuariosCtrl.singIn);
+  api.post('/login/intranet',                usuariosCtrl.singInEmpleado);
 
   api.get('/valoraciones',                   valoracionesCtrl.getValoraciones);
   api.post('/valoraciones',                  valoracionesCtrl.saveValoracion);
