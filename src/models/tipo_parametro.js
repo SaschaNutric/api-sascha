1043557b1a11efb4e2d52dc6c312bdc0f47d9302
Sascha <37225508+SaschaNutric@users.noreply.github.com>
2018-05-14 22:56:44 -0400

    Actualizando tipo parametros

--- src/models/tipo_parametro.js
+++ src/models/tipo_parametro.js
@@ -1,10 +1,14 @@
 'use strict'
 
 const Bookshelf = require('../commons/bookshelf');
+require('./parametro');
 
 let TipoParametro = Bookshelf.Model.extend({
   tableName: 'tipo_parametro',
-  idAttribute: 'id_tipo_parametro'
+  idAttribute: 'id_tipo_parametro',
+    parametros: function() {
+  	return this.hasMany('Parametro', 'id_tipo_parametro');
+    }
 });
 
 module.exports = Bookshelf.model('TipoParametro', TipoParametro);
