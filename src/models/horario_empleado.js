6a1e915f50d3e82126b443056e7aba0d75e0d401
Leo1305Pineda <leo1305pineda@gmail.com>
2018-05-16 06:37:42 -0400

    Asignando Bloque horario al empleado

--- src/models/horario_empleado.js
+++ src/models/horario_empleado.js
@@ -9,7 +9,7 @@ let Horario_empleado = Bookshelf.Model.extend({
   tableName: 'horario_empleado',
   idAttribute: 'id_horario_empleado',
   empleado: function(){
-    return this.hasMany(Empleado, 'id_empleado'); 
+    return this.belongsTo(Empleado, 'id_empleado'); 
   },
   bloque_horario: function(){
     return this.hasMany(Bloque_horarios, 'id_bloque_horario');
