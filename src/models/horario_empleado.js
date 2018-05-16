05fb7b9d70aa03dd8906b2693bc2ae6299fee3b1
Leo1305Pineda <leo1305pineda@gmail.com>
2018-05-16 07:21:00 -0400

    Asignando Bloque horario al empleado

--- src/models/horario_empleado.js
+++ src/models/horario_empleado.js
@@ -1,22 +1,21 @@
 'use strict'
 
 const Bookshelf 		= require('../commons/bookshelf');
-const Bloque_horarios 	= require('../collections/bloque_horarios');
-const Dia_laborables 	= require('../collections/dia_laborables');
+const Bloque_horario 	= require('./bloque_horario');
+const Dia_laborable 	= require('./dia_laborable');
 const Empleado 			= require('./empleado');
 
 let Horario_empleado = Bookshelf.Model.extend({
   tableName: 'horario_empleado',
-  idAttribute: 'id_horario',
-  idAttribute: 'id_empleado',
+  idAttribute: 'id_horario_empleado',
   empleado: function(){
-    return this.belongsTo(Empleado, 'id_empleado'); 
+    return this.belongsTo(Empleado, 'id_empleado');
   },
-  bloque_horarios: function(){
-    return this.hasMany(Bloque_horarios, 'id_bloque_horario');
+  bloque_horario: function(){
+    return this.belongsTo(Bloque_horario, 'id_bloque_horario');
   },
-  dia_laborables: function(){
-    return this.hasMany(Dia_laborables, 'id_dia_laborable');
+  dia_laborable: function(){
+    return this.belongsTo(Dia_laborable, 'id_dia_laborable');
   }
 });
 
