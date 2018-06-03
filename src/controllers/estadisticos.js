a3e80087e468be3658b025c0ca8981dd561fdd59
SaschaNutric <saschanutric@gmail.com>
2018-06-03 17:41:33 -0400

    reporte estadistico de reclamo

--- src/controllers/estadisticos.js
+++ src/controllers/estadisticos.js
@@ -3,7 +3,8 @@
 const Bookshelf = require('../commons/bookshelf');
 const VistaEstadisticoClientes = require('../collections/vista_estadistico_clientes');
 const VistaNutricionistas = require('../collections/vista_nutricionistas');
-const VistaCanalEscuchas = require('../collections/vista_canal_escuchas')
+const VistaCanalEscuchas = require('../collections/vista_canal_escuchas');
+const VistaReclamos = require('../collections/vista_reclamos')
 
 
 function getMotivosSolicitudPreferidos(req, res, next) {
@@ -74,42 +75,42 @@ function getMotivosSolicitudPreferidos(req, res, next) {
 
 }
 
-function getVisitasByNutricionista(req, res, next){
+function getVisitasByNutricionista(req, res, next) {
     let id_especialidad = req.body.id_especialidad || null
     let rango_fecha = {
-        minimo: req.body.fecha_inicial || null ,
-        maximo : req.body.fecha_final || null
+        minimo: req.body.fecha_inicial || null,
+        maximo: req.body.fecha_final || null
     }
 
-    VistaNutricionistas.query( function(qb){
+    VistaNutricionistas.query(function (qb) {
         qb.select('nombre_empleado');
         qb.countDistinct(' id_orden_servicio as cantidad_clientes');
-        if(id_especialidad != null)
-            qb.where('id_especialidad',id_especialidad)
+        if (id_especialidad != null)
+            qb.where('id_especialidad', id_especialidad)
         if (rango_fecha.minimo && rango_fecha.maximo)
             qb.where('fecha_creacion', '>=', rango_fecha.minimo)
                 .andWhere('fecha_creacion', '<=', rango_fecha.maximo);
-        qb.groupBy( 'nombre_empleado')
-        qb.orderBy('cantidad_clientes','DESC')
+        qb.groupBy('nombre_empleado')
+        qb.orderBy('cantidad_clientes', 'DESC')
 
     })
-    .fetch()
-    .then(function(data){
-        data.map(function(emp){
+        .fetch()
+        .then(function (data) {
+            data.map(function (emp) {
+
+            })
+            return res.status(200).json({ error: false, data: data });
 
         })
-        return res.status(200).json({ error: false, data: data });
-        
-    })
-    .catch(function (err) {
-        return res.status(500).json({ error: true, data: { mensaje: err.message } });
-    });
+        .catch(function (err) {
+            return res.status(500).json({ error: true, data: { mensaje: err.message } });
+        });
 
 
 }
 
 
-function getMotivosByTipoContacto(req,res,next){
+function getMotivosByTipoContacto(req, res, next) {
     if (!req.body.id_tipo_motivo) {
         return res.status(400).json({
             error: true,
@@ -123,7 +124,54 @@ function getMotivosByTipoContacto(req,res,next){
         id_rango_edad: req.body.id_rango_edad || null
 
     }
-    
+
+    let rango_fecha = {
+        minimo: req.body.fecha_inicial || null,
+        maximo: req.body.fecha_final || null
+    }
+
+    let filtros = new Object();
+    for (let item in campos) {
+        if (campos.hasOwnProperty(item)) {
+            if (campos[item] != null)
+                filtros[item] = campos[item];
+        }
+    }
+    console.log(filtros)
+    VistaCanalEscuchas.query(function (qb) {
+        qb.select('motivo_descripcion');
+        qb.count('id_comentario as cantidad');
+        qb.where('id_tipo_motivo', req.body.id_tipo_motivo)
+        qb.where(filtros);
+        if (rango_fecha.minimo && rango_fecha.maximo)
+            qb.where('fecha_creacion', '>=', rango_fecha.minimo)
+                .andWhere('fecha_creacion', '<=', rango_fecha.maximo);
+        qb.groupBy('motivo_descripcion')
+        qb.orderBy('cantidad', 'DESC')
+    })
+        .fetch()
+        .then(function (data) {
+            return res.status(200).json({ error: false, data: data });
+
+        })
+        .catch(function (err) {
+            return res.status(500).json({ error: true, data: { mensaje: err.message } });
+        });
+
+
+}
+
+function getReclamosByRespuesta(req, res, next) {
+    let campos = {
+        id_especialidad: req.body.id_especialidad || null,
+        id_servicio: req.body.id_servicio || null,
+        id_genero: req.body.id_genero || null,
+        id_estado_civil: req.body.id_estado_civil || null,
+        id_rango_edad: req.body.id_rango_edad || null,
+        id_empleado: req.body.id_empleado || null
+    }
+
+
     let rango_fecha = {
         minimo: req.body.fecha_inicial || null,
         maximo: req.body.fecha_final || null
@@ -137,31 +185,42 @@ function getMotivosByTipoContacto(req,res,next){
         }
     }
 
-    VistaCanalEscuchas.query(function(qb){
-        qb.select('motivo_descripcion');
-        qb.count('id_comentario as cantidad');
-        qb.where('id_tipo_motivo', req.body.id_tipo_motivo)
-        qb.where(filtros);
-        if (rango_fecha.minimo && rango_fecha.maximo)
-        qb.where('fecha_creacion', '>=', rango_fecha.minimo)
-            .andWhere('fecha_creacion', '<=', rango_fecha.maximo);
-        qb.groupBy('motivo_descripcion')
-        qb.orderBy('cantidad', 'DESC')
-    })
-    .fetch()
-    .then(function(data){
-        return res.status(200).json({ error: false, data:data });
-        
-    })
-    .catch(function (err) {
-        return res.status(500).json({ error: true, data: { mensaje: err.message } });
-    });
+    console.log(filtros);
 
+    let query = `select motivo_descripcion, 
+                sum(CASE  WHEN aprobado     
+                       THEN 1  
+                 		ELSE 0
+                  END) as aprobados,
+             sum(CASE  WHEN aprobado      
+                        THEN 0  
+                  		ELSE 1
+                   END) as rechazados
+                from vista_reclamo
+                WHERE `
+    if (filtros.id_genero) query+= `id_genero = ${filtros.id_genero} AND `
+    if (filtros.id_especialidad)  query+= `id_especialidad = ${filtros.id_especialidad} AND `
+    if (filtros.id_servicio)  query+= `id_servicio= ${filtros.id_servicio} AND `
+    if (filtros.id_rango_edad)  query+= `id_rango_edad = ${filtros.id_rango_edad} AND `
+    if (filtros.id_empleado)  query+= `id_empleado = ${filtros.id_empleado} AND `
+    if (filtros.id_estado_civil)  query+= `id_estado_civil = ${filtros.id_estado_civil} AND `
+    if (rango_fecha.minimo && rango_fecha.maximo)  query+= `fecha_creacion >= ${rango_fecha.minimo} AND fecha_creacion >=  ${rango_fecha.maximo} AND `
+    query+=` id_reclamo > 0  group by motivo_descripcion`
+    console.log(query)
+    Bookshelf.knex.raw(query)
+        .then(function (data) {
+            return res.status(200).json({ error: false, data: data });
+
+        }).catch(function (err) {
+            return res.status(500).json({ error: true, data: { mensaje: err.message } });
+        });
 
 }
+
 
 module.exports = {
     getMotivosSolicitudPreferidos,
     getVisitasByNutricionista,
-    getMotivosByTipoContacto
+    getMotivosByTipoContacto,
+    getReclamosByRespuesta
 }
